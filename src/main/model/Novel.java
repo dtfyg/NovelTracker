6e4b197a75f3cd33a1b1ce73217a144a0c5e0093
UBC Student <yiran10@students.cs.ubc.ca>
2021-07-24 19:18:03 -0700

    Implemented a model, Novel and its tests as well as descriptions for each method

---
+++ src/main/model/Novel.java
@@ -0,0 +1,98 @@
+package model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Novel {
+
+    public static final String genre1 = "Fantasy";
+    public static final String genre2 = "Sci-Fi";
+    public static final String genre3 = "Romance";
+    public static final String genre4 = "Comedy";
+    public static final String genre5 = "Adventure";
+    public static final String genre6 = "Yaoi/Yuri";
+    public static final String genre7 = "LitRPG";
+    public static final String genre8 = "Tragedy";
+
+    private String name;
+
+    private ArrayList<String> genre;
+
+    private double rating;
+
+    //Effects: Create a new novel with a name
+    public Novel(String name) {
+        this.name = name;
+        this.rating = -1;
+        this.genre = new ArrayList<>();
+    }
+
+    //Modifies: This
+    //Effects: Adds a rating for the novel
+    public void rateNovel(double rating) {
+        this.rating = rating;
+    }
+
+
+    //Effects: Gets the rating of the novel
+    public double getRating() {
+        return this.rating;
+    }
+
+
+    //Modifies: This
+    //Effects: Adds a genre to the novel
+    public void addGenre(int genre) {
+        switch (genre) {
+            case 1: this.genre.add(genre1);
+                break;
+            case 2: this.genre.add(genre2);
+                break;
+            case 3: this.genre.add(genre3);
+                break;
+            case 4: this.genre.add(genre4);
+                break;
+            case 5: this.genre.add(genre5);
+                break;
+            case 6: this.genre.add(genre6);
+                break;
+            case 7: this.genre.add(genre7);
+                break;
+            case 8: this.genre.add(genre8);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Requires: this novel has a genre
+    //Modifies: this
+    //Effects: Removes the genre from this novel and return true,
+    // otherwise return false if the genre does not exist
+    public boolean removeGenre(String genre) {
+        if (this.genre.contains(genre)) {
+            this.genre.remove(genre);
+            return true;
+        }
+        return false;
+    }
+
+    //Requires: this novel has a genre
+    //Effects: Returns all the genres of the Novel
+    public ArrayList<String> getGenre() {
+        return this.genre;
+    }
+
+    //Modifies: This
+    //Effects: Changes the name of the novel
+    public void changeName(String newName) {
+        this.name = newName;
+    }
+
+    //Effects: Gets the name
+    public String getName() {
+        return this.name;
+    }
+
+}
---
+++ src/test/model/NovelTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NovelTest {
+
+    Novel n1;
+    Novel n2;
+    Novel n3;
+    ArrayList<String> s;
+
+    @BeforeEach
+    public void setup(){
+        n1 = new Novel("Harry Potter and the Philosopher's Stone");
+        n2 = new Novel("Mother of Learning");
+        n3 = new Novel("Beware of Chicken");
+    }
+
+    @Test
+    public void testAddRating(){
+        n1.rateNovel(3);
+        assertEquals(3, n1.getRating());
+    }
+
+
+    @Test
+    public void testAddGenre(){
+        n3.addGenre(2);
+        s = new ArrayList<>();
+        s.add(Novel.genre2);
+        assertEquals(s, n3.getGenre());
+    }
+
+    @Test
+    public void testAddMultipleGenre(){
+        n3.addGenre(4);
+        n3.addGenre(1);
+        n3.addGenre(5);
+        n3.addGenre(6);
+        n3.addGenre(3);
+        n3.addGenre(7);
+        n3.addGenre(8);
+        s = new ArrayList<>();
+        s.add(Novel.genre4);
+        s.add(Novel.genre1);
+        s.add(Novel.genre5);
+        s.add(Novel.genre6);
+        s.add(Novel.genre3);
+        s.add(Novel.genre7);
+        s.add(Novel.genre8);
+        assertEquals(s, n3.getGenre());
+    }
+
+    @Test
+    public void testRemoveGenreSuccess(){
+        n3.addGenre(4);
+        assertTrue(n3.removeGenre("Comedy"));
+        s = new ArrayList<>();
+        assertEquals(s, n3.getGenre());
+    }
+
+    @Test
+    public void testRemoveGenreFail(){
+        n3.addGenre(4);
+        assertFalse(n3.removeGenre("Adventure"));
+    }
+
+    @Test
+    public void testNameChange(){
+        n3.changeName("Chicken Attack!");
+        assertEquals("Chicken Attack!", n3.getName());
+    }
+
+}
