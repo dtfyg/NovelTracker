962b56e727182ee592b67db1584ce2566a79bd2c
UBC Student <yiran10@students.cs.ubc.ca>
2021-08-20 15:23:18 -0700

    Adding more songs

--- src/main/ui/MusicThread.java
+++ src/main/ui/MusicThread.java
@@ -5,13 +5,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class MusicThread implements Runnable {
     private Clip clipC;
     private boolean star = false;
     private List<String> playlist = new ArrayList<>();
+    int prev;
 
     public MusicThread() {
         init();
@@ -20,7 +20,10 @@ public class MusicThread implements Runnable {
     @Override
     public void run() {
         try {
-            int n = (int) (Math.random() * 6) + 1;
+            int n = (int) (Math.random() * 7) + 1;;
+            while (prev == n) {
+                n = (int) (Math.random() * 7) + 1;
+            }
             CountDownLatch syncLatch = new CountDownLatch(1);
             AudioInputStream sound = listOfSongs(n);
 
@@ -30,19 +33,25 @@ public class MusicThread implements Runnable {
                     syncLatch.countDown();
                 }
             });
-            System.out.println("Current Song: " + playlist.get(n - 1));
-            System.out.println("-----------------------------------------");
+            System.out.println("Current Song: " + n + ". " + playlist.get(n - 1));
+            System.out.println("-------------------------------------------");
             clipC.open(sound);
             clipC.start();
             syncLatch.await();
+            prev = n;
             run();
+        } catch (UnsupportedAudioFileException u) {
+            System.out.println("Unsupported Audio");
+        } catch (IOException i) {
+            System.out.println("IOException");
         } catch (Exception e) {
-            System.out.println("Sound exception");
+            System.out.println("Random Error");
         }
 
     }
 
 
+    @SuppressWarnings("checkstyle:MethodLength")
     public AudioInputStream listOfSongs(int n) throws UnsupportedAudioFileException, IOException {
         AudioInputStream s;
         switch (n) {
@@ -61,6 +70,9 @@ public class MusicThread implements Runnable {
             case 5:
                 s = AudioSystem.getAudioInputStream(new File("./data/ToTheMoon.wav"));
                 break;
+            case 6:
+                s = AudioSystem.getAudioInputStream(new File("./data/Anima.wav"));
+                break;
             default:
                 s = AudioSystem.getAudioInputStream(new File("./data/Tetris99.wav"));
                 break;
@@ -74,6 +86,7 @@ public class MusicThread implements Runnable {
         playlist.add("Ori and the Blind Forest - Lost in the Storm");
         playlist.add("Ori and the Blind Forest - Restoring the Light, Facing the dark");
         playlist.add("To The Moon - Ending Theme");
+        playlist.add("Deemo - Anima");
         playlist.add("Tetris 99 - Main Theme");
     }
 
